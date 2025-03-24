01757a407ca429d418e956f3bdb382d357be9835
DwiSetiawati <dwisetwa27@gmail.com>
2025-03-25 00:15:18 +0700

    Fungsi Display (void display)

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -54,3 +54,19 @@ void insertionSort()
         arr[j + 1] = temp; // Step 5
     }
 }
+
+void display()
+{
+    cout << endl; // output baris kosong
+    cout << "==================================" << endl;
+    cout << "Total Pass = " << n - 1 << endl;             // count element movement
+    cout << "==================================" << endl; // output ke layar
+    cout << "Element Array yang telah tersusun" << endl;  // output ke layar
+    cout << "==================================" << endl; // output ke layar
+
+    for (int j = 0; j < n; j++)
+    {                           // Looping dengan j dimulai dari 0 hinnga n-1
+        cout << arr[j] << endl; // output ke layar
+    }
+    cout << endl; // Output baris kosong
+}
